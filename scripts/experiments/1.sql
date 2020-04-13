049d6d3c31aa619fb66f6f516b3f9c1b4105e1ab
Mateusz Najda <mateusznajda@gmail.com>
2020-04-13 08:44:33 +0200

    added experiments for testing halting property and order of execution

---
+++ scripts/experiments/1.sql
@@ -0,0 +1,28 @@
+create table foo (bar varchar2(255), baz timestamp);
+
+create or replace trigger triggerA after insert on foo
+begin 
+    insert into foo values ('A', current_timestamp);
+    dbms_output.put_line('A');
+end;
+/
+
+create or replace trigger triggerB after insert on foo 
+begin
+    insert into foo values ('B', current_timestamp);
+    dbms_output.put_line('B');
+end;
+/
+
+create or replace trigger triggerC after insert on foo
+begin
+    insert into foo values ('C', current_timestamp);
+    dbms_output.put_line('C');
+end;
+/
+
+insert into foo values ('test', current_timestamp);
+
+select * from foo;
+
+drop table foo;
---
+++ scripts/experiments/2.sql
@@ -0,0 +1,25 @@
+create table foo (bar varchar2(255));
+
+create or replace trigger triggerA after insert on foo
+begin 
+    dbms_output.put_line('A');
+end;
+/
+
+create or replace trigger triggerB after insert on foo
+follows triggerA
+begin
+    dbms_output.put_line('B');
+end;
+/
+
+create or replace trigger triggerC after insert on foo
+follows triggerB
+begin
+    dbms_output.put_line('C');
+end;
+/
+
+insert into foo values ('test');
+
+drop table foo;
