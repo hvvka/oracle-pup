9827e3307978728ebbcc7fad251796d7ac00420d
Hanna Grodzicka <hania.grodzicka@gmail.com>
2020-05-17 22:53:02 +0200

    Add old docs and python script to convert airport to geo data

---
+++ scripts/geo/convert_to_geo_data.py
@@ -0,0 +1,20 @@
+import fileinput
+
+AIRPORT_FILE = "airport_geo.sql"
+
+
+def convert_airport(filename):
+    with fileinput.input(filename, inplace=True) as f:
+        for line in f:
+            x = float(line.split("'")[-8])
+            y = float(line.split("'")[-6])
+            line = line.replace("'{}','{}'".format(x, y),
+                                "MDSYS.SDO_GEOMETRY(2001,8307,MDSYS.SDO_POINT_TYPE({},{},NULL),NULL,NULL)".format(x, y))
+            line = line.replace(");",
+                                ",MDSYS.SDO_GEOMETRY(2003,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,4),"
+                                "MDSYS.SDO_ORDINATE_ARRAY({},{}, {},{}, {},{})));".format(
+                                    x - 1, y - 2, x + 1, y, x - 1, y + 2))
+            print(line, end='')
+
+
+convert_airport(AIRPORT_FILE)
