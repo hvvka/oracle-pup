cbb237870cd5cbf33ad06d6a3dbad5e87d4e4382
Hanna Grodzicka <hania.grodzicka@gmail.com>
2020-05-17 23:39:35 +0200

    Convert flight table to geo data

--- scripts/geo/convert_to_geo_data.py
+++ scripts/geo/convert_to_geo_data.py
@@ -1,6 +1,8 @@
 import fileinput
+from random import uniform
 
 AIRPORT_FILE = "airport_geo.sql"
+FLIGHT_FILE = "flight_geo.sql"
 
 
 def convert_airport(filename):
@@ -17,4 +19,23 @@ def convert_airport(filename):
             print(line, end='')
 
 
+def new_point():
+    return uniform(-180, 180), uniform(-90, 90)
+
+
+def convert_flight(filename):
+    with fileinput.input(filename, inplace=True) as f:
+        for line in f:
+            to_be_appended = line.split(",'")[1]
+
+            x, y = new_point()
+            from_x, from_y = new_point()
+            destination = ",MDSYS.SDO_GEOMETRY(2001, 8307, MDSYS.SDO_POINT_TYPE({}, {}, NULL), NULL, NULL)".format(x, y)
+            route = ",MDSYS.SDO_GEOMETRY(2002,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1),MDSYS.SDO_ORDINATE_ARRAY(" \
+                    "{},{}, {},{}))".format(from_x, from_y, x, y)
+
+            print(line.replace(to_be_appended, to_be_appended + destination + route), end='')
+
+
 convert_airport(AIRPORT_FILE)
+convert_flight(FLIGHT_FILE)
