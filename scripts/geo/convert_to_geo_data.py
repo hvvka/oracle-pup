e3fa5cd1afa6b4ae3841b52fe7f0e84fe641cae1
Hanna Grodzicka <hania.grodzicka@gmail.com>
2020-05-19 11:53:14 +0200

    Replace 2nd geo property with null

--- scripts/geo/convert_to_geo_data.py
+++ scripts/geo/convert_to_geo_data.py
@@ -11,9 +11,9 @@ def convert_airport(filename):
             x = float(line.split("'")[-8])
             y = float(line.split("'")[-6])
             line = line.replace("'{}','{}'".format(x, y),
-                                "MDSYS.SDO_GEOMETRY(2001,8307,MDSYS.SDO_POINT_TYPE({},{},NULL),NULL,NULL)".format(x, y))
+                                "MDSYS.SDO_GEOMETRY(2001,NULL,MDSYS.SDO_POINT_TYPE({},{},NULL),NULL,NULL)".format(x, y))
             line = line.replace(");",
-                                ",MDSYS.SDO_GEOMETRY(2003,8307,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,4),"
+                                ",MDSYS.SDO_GEOMETRY(2003,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,4),"
                                 "MDSYS.SDO_ORDINATE_ARRAY({},{}, {},{}, {},{})));".format(
                                     x - 1, y - 2, x + 1, y, x - 1, y + 2))
             print(line, end='')
@@ -30,7 +30,7 @@ def convert_flight(filename):
 
             x, y = new_point()
             from_x, from_y = new_point()
-            destination = ",MDSYS.SDO_GEOMETRY(2001,8307,MDSYS.SDO_POINT_TYPE({}, {}, NULL),NULL,NULL)".format(x, y)
+            destination = ",MDSYS.SDO_GEOMETRY(2001,NULL,MDSYS.SDO_POINT_TYPE({}, {}, NULL),NULL,NULL)".format(x, y)
             route = ",MDSYS.SDO_GEOMETRY(2002,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1),MDSYS.SDO_ORDINATE_ARRAY(" \
                     "{},{}, {},{}))".format(from_x, from_y, x, y)
 
